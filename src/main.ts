c001d56ac1fec4290eba66834ece319f6441e7be
Brandyn Britton <brandyn.britton@consent2go.com>
2024-02-02 14:17:07 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div style="min-height: 100vh; min-width: 100vw; overflow: hidden; display: flex; justify-content: center; align-items: center" >
+    <h1>You are probably looking for <a href="https://jsworldconference.com/">https://jsworldconference.com</a></h1>
+  </div>
+`
