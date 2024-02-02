ebe2607398a9961306d868b7333264cfc5d9233a
Brandyn Britton <brandyn.britton@consent2go.com>
2024-02-02 14:28:41 +0800

    update: little bit of formatting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div style="min-height: 100vh; min-width: 100vw; overflow: hidden; display: flex; justify-content: center; align-items: center" >
+  <div style="min-height: 100vh; min-width: 100vw; text-align: center; overflow: hidden; display: flex; justify-content: center; align-items: center" >
     
-    <h1>Womp 🐸. You are probably looking for <a href="https://jsworldconference.com/">https://jsworldconference.com</a></h1>
+    <h1>Womp 🐸<br/> You are probably looking for <a href="https://jsworldconference.com/">https://jsworldconference.com</a></h1>
   </div>
 `
